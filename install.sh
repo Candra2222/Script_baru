432a4ee39804fa145182e6bc9a41301cb215b670
Candra2222 <84482710+Candra2222@users.noreply.github.com>
2022-07-16 07:16:17 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+pkg update && pkg upgrade
+pkg install mpv -y
+pkg install play-audio -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install python -y
+pkg install pip -y
+pkg install git -y
+pkg install curl -y
+
+
+pip install requests
+pip install bs4
+pip install futures
+pip install mechanize
+pip install ipaddress
+pip install hashlib
+python3 menu.py
